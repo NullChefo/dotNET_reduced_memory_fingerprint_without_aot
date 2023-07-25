ef572ac3553eac8576214a34fce22b6f0fffa866
nullchefo <stefan.kehayov.96@gmail.com>
2023-07-25 18:35:00 +0300

    first commit

---
+++ DOT_NET_DOCKER_SIZE_NOT_AOT/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace DOT_NET_DOCKER_SIZE_NOT_AOT;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
